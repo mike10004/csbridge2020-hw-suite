952554ce46b7e80e1164afb7319161945e2c6d8c
Mike Chaberski <mchaberski@gmail.com>
2020-03-07 19:57:01 -0500

    test_module: fix do_init calls

--- hwsuite/tests/test_module.py
+++ hwsuite/tests/test_module.py
@@ -19,13 +19,13 @@ class ModuleMethodsTest(TestCase):
 
     def test_find_proj_root_standard(self):
         with tempfile.TemporaryDirectory() as tempdir:
-            hwsuite.init.do_init(tempdir, 'unittest-example', safety_mode='abort')
+            hwsuite.init.do_init(tempdir, safety_mode='cautious', hwconfig={})
             actual = hwsuite.find_proj_root(cwd=tempdir)
             self.assertEqual(tempdir, actual)
 
     def test_find_proj_root_child(self):
         with tempfile.TemporaryDirectory() as tempdir:
-            hwsuite.init.do_init(tempdir, 'unittest-example', safety_mode='abort')
+            hwsuite.init.do_init(tempdir, safety_mode='cautious', hwconfig={})
             subdir = os.path.join(tempdir, "q1")
             os.makedirs(subdir)
             actual = hwsuite.find_proj_root(cwd=subdir)
@@ -34,7 +34,7 @@ class ModuleMethodsTest(TestCase):
     def test_store_config(self):
         with tempfile.TemporaryDirectory() as tempdir:
             proj_root = tempdir
-            hwsuite.init.do_init(proj_root, 'unittest-example', safety_mode='abort')
+            hwsuite.init.do_init(proj_root, safety_mode='cautious', hwconfig={})
             cfg = hwsuite.get_config(proj_root=proj_root)
             cfg['foo'] = 'bar'
             hwsuite.store_config(cfg, proj_root=proj_root)
