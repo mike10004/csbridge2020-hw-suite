2c5c94f94f778967f0109bd807638a9381ebebdb
Mike Chaberski <mchaberski@gmail.com>
2020-03-07 20:27:54 -0500

    test_module: fix store_config test

--- hwsuite/tests/test_module.py
+++ hwsuite/tests/test_module.py
@@ -36,10 +36,12 @@ class ModuleMethodsTest(TestCase):
             proj_root = tempdir
             hwsuite.init.do_init(proj_root, safety_mode='cautious', hwconfig={})
             cfg = hwsuite.get_config(proj_root=proj_root)
+            expected_cfg = dict(cfg.items())
             cfg['foo'] = 'bar'
+            expected_cfg['foo'] = 'bar'
             hwsuite.store_config(cfg, proj_root=proj_root)
             cfg = hwsuite.get_config(proj_root=proj_root)
-            self.assertDictEqual({'foo': 'bar'}, cfg)
+            self.assertDictEqual(expected_cfg, cfg)
 
     def test_describe_path(self):
         test_cases = [
